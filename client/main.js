217ed99cb832d1c0847bd39954df44c27a2bf7b8
lpatmo <linda.peng@alumni.duke.edu>
2016-10-27 21:03:53 -0700

    added bootstrap package and changed template name

--- client/main.js
+++ client/main.js
@@ -3,20 +3,19 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.questions.helpers({
+  
+  questions: function() {
+    return Questions.find();
+  }
+  
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
 });
